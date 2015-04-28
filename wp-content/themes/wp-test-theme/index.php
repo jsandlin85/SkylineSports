6bd7bd8f444225fa9ca1b701607be535c92575ac
Jeremy Sandlin <jeremy.sandlin@mercurycsc.com>
2015-04-28 13:43:55 -0600

    Adds a header to the theme index.php as a test

--- wp-content/themes/wp-test-theme/index.php
+++ wp-content/themes/wp-test-theme/index.php
@@ -16,6 +16,8 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
+		<h1>I am Adding this as a test.</h1>
+
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
